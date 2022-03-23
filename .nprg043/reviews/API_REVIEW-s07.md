6a36f36a8a4a1e9f526543e26a6d993d3cccb472
Lubomír Bulej <bulej@d3s.mff.cuni.cz>
2022-03-23 15:55:07 +0100

    Import API review submission from s07

--- .nprg043/reviews/API_REVIEW-s07.md
+++ .nprg043/reviews/API_REVIEW-s07.md
@@ -1,1 +1,53 @@
-The reviewer did not submit a review!
+﻿# Prvý dojem
+Knižnica ArgParser na prvý dojem pôsobí kvalitne a je vidieť, že si autori dali
+záležať na jej návrhu. Hlavnými črtmi už teraz pôsobí tak, že podporuje parsovanie 
+argumentov pre veľkú škálu command-line programov s rozličnými typmi argumentov
+a ich parametrami.
+Repozitár knižnice je dobre štruktúrovaný a obsahuje aj niekoľko užitočných 
+ukážkových aplikácií, ktoré zjednodušujú jej prvotné použitie. Taktiež nechýba
+ani základná dokumentácia k správnemu použitiu a inštrukcie k integrácii knižnice 
+do existujúcich projektov.
+
+# API review
+Knižnica prenáša na seba svojim API veľkú časť zodpovednosti čo sa týka 
+zadefinovania argumentov, kontroly ich správnosti a taktiež uloženia naparsovaných hodnôt.
+Týmto užívatelovi celkovo spríjemnuje jej použitie k parsovaniu argumentov v jeho programe.
+Uživatel sa nemusí starať o typovú kontrolu a k hodnotám argumentov sa vie jednoducho dostať
+dostať cez objekt parsera.
+
+Samotné zadefinovanie parsera a jeho použitie zabraňuje zbytočným chýbam 
+už pri kompilácii programu vďaka silne typovanému prístupu k hodnotám argumentov.
+API knižnice taktiež rieši aj chybové stavy a to tak, že jednotlivé metódy vyhadzujú správne
+výnimky, čo dáva užívatelovi možnosť na tieto chybové stavy nejakým spôsobom reagovať.
+
+Dokumentácia API je z veľkej časti dostatočná, no nájdu sa niektoré časti API, kde nie je 
+úplne zrejmé, čo daná vec robí.
+
+# numactl
+Použitie knižnice výrazne zjednodušilo a zrýchlilo implementáciu
+parsovania argumentov pre program numactl. 
+Zadefinovanie argumentov programu ako členov triedy dediacej z `ParserBase` 
+je praktické a vnieslo do kódu prehladnosť podporovaných argumentov. Taktiež to
+umožnilo veľmi jednoduchú prácu s jednotlivými argumentami a ich naparsovanými 
+hodnotami.
+
+Táto implementácia však odhalila aj niekoľko nedostatkov. Parsovania argumentu `args`, ktorý 
+má obsahovať argumenty pre spustenie príkazu zadaného v argumente `command`, sa nedalo
+vôbec naimplementovať. Ak knižnica toto nejakým spôsobom nakoniec podporuje, nie je 
+to vôbec zrejmé z jej dokumentácie. Ďalej pri definicii argumentov sa nedal zadať názov pre
+jednotlivé parametre argumentov - napríklad pri možnosti `preferred` je názov parametru `<node>`.
+A ako posledná vec, ktorá mierne znepríjemnila implementáciu v numactl, sú vzájomne vylučujúce sa
+options `-m, -p, -i`. Knižnica s týmto nijako nepomohla a skontrolovanie výlučnosti tak bolo ponechané 
+na užívateľa knižnice.
+
+# Doplňujúci komentár
+Nedostatky:
+- Nemožnosť získania počtu naparsovaných parametrov pre prípady, kedy je počet parametrov ľubovolný. 
+Taktiež sa nedá ani získať všetky naparsované parametre naraz ako jednu kolekciu.
+- Chýba nastavitelnosť vzájmne vylučujúcich sa argumentov.
+- Chýba nastavitelnosť názvov pre jednotlivé parametre v argumentoch.
+- Jednotlivé triedy by možno bolo vhodné rozdeliť do samostatných súborov. Teraz je mnoho tried nakope 
+a celkom ťažko sa orientuje v kóde.
+- Nie je úplne jasné z dokumentácie rozdelenie medzi Argument a Option. Tieto triedy vyzerajú veľmi
+podobne - nedá sa to nejako zjednotiť v prípade, že sa tak veľmi od seba nelíšia?
+
---
+++ src/Numactl/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using ArgParser;
+
+namespace Numactl
+{
+    class Parser : ParserBase
+    {
+        public NoValueOption show = new(new string[] { "S", "show" }, "Show current NUMA policy.");
+        public NoValueOption hardware = new(new string[] { "H", "hardware" }, "Print hardware configuration.");
+        
+        // Name of param (e.g. <node>) cannot be set for generating help
+        public IntOption preferred = new(new string[] { "p", "preferred" }, 
+            "Prefer memory allocations from given node.", parameterAccept: ParameterAccept.Mandatory);
+
+        public StringOption interleave = new(new string[] { "i", "interleave" },
+            "Interleave memory allocation across given nodes.", ParameterAccept.Mandatory);
+
+        public StringOption membind = new(new string[] { "m", "membind" },
+            "Allocate memory from given nodes only.", ParameterAccept.Mandatory);
+
+        public StringOption physcpubind = new(new string[] { "C", "physcpubind" },
+            "Run on given CPUs only.", ParameterAccept.Mandatory);
+
+        public StringArgument command = new("command", "", ParameterAccept.Mandatory);
+        public StringArgument args = new("args", "", ParameterAccept.Any);
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var parser = new Parser();
+            try
+            {
+                parser.Parse(args);
+            } catch (ParseException)
+            {
+                Console.WriteLine("Invalid arguments");
+                return;
+            } catch (ParserCodeException)
+            {
+                Console.WriteLine("Invalid parser definition");
+                return;
+            }
+
+            // What IsSet actually does?
+            // Parser could maybe somehow support exlusivness of specific options to avoid the big condition below.
+            if ((parser.preferred.GetValue() != null && (parser.interleave.IsSet || parser.membind.IsSet)) ||
+                (parser.interleave.IsSet && (parser.preferred.GetValue() != null || parser.membind.IsSet)) ||
+                (parser.membind.IsSet && (parser.interleave.IsSet || parser.preferred.GetValue() != null)))
+            {
+                Console.WriteLine("Options are in conflict");
+                return;
+            }
+
+            if (parser.show.GetValue())
+            {
+                PrintPolicy(parser);
+            }
+            else if (parser.hardware.GetValue())
+            {
+                PrintHardwareConfiguration();
+            }
+            else if (parser.command.GetValue() != null)
+            {
+                Console.WriteLine("Running command: " + parser.command.GetValue());
+                // How to access all args if we do not know the final number of them?
+                Console.WriteLine("Command args: " + parser.args.GetValue());
+                PrintPolicy(parser);
+            } else
+            {
+                Console.WriteLine(parser.GenerateHelp());
+            }
+        }
+
+        private static void PrintPolicy(Parser parser)
+        {
+            Console.WriteLine("policy: default");
+            Console.WriteLine("preferred node: " + parser.preferred.GetValue());
+            Console.WriteLine("physcpubind: " + parser.physcpubind.GetValue());
+            Console.WriteLine("cpubind: 0 1");
+            Console.WriteLine("nodebind: " + parser.preferred.GetValue());
+            Console.WriteLine("membind: " + parser.membind.GetValue());
+        }
+
+        private static void PrintHardwareConfiguration()
+        {
+            Console.WriteLine("available: 2 nodes (0-1)");
+            Console.WriteLine("node 0 cpus: 0 2 4 6 8 10 12 14 16 18 20 22");
+            Console.WriteLine("node 0 size: 24189 MB");
+            Console.WriteLine("node 0 free: 18796 MB");
+            Console.WriteLine("node 1 cpus: 1 3 5 7 9 11 13 15 17 19 21 23");
+            Console.WriteLine("node 1 size: 24088 MB");
+            Console.WriteLine("node 1 free: 16810 MB");
+            Console.WriteLine("node distances:");
+            Console.WriteLine("node   0   1");
+            Console.WriteLine("  0:  10  20");
+            Console.WriteLine("  1:  20  10");
+        }
+    }
+}
